c1e66cb85b2c1db0ddb081c7e2652c45792a3241
urnm1 <54842038+urnm1@users.noreply.github.com>
2019-09-07 19:13:55 +0300

    Update 1df3034d9.hpp

--- 1df3034d9.hpp
+++ 1df3034d9.hpp
@@ -1,1 +1,29 @@
 
+#ifndef PRJ_NS1_NS2_CLASS_HPP
+#define PRJ_NS1_NS2_CLASS_HPP
+
+namespace ns1 {
+namespace ns2 {
+
+class IInterface1
+{
+public:
+  virtual ~IInterface1() = default;
+};
+
+class IInterface11
+{
+public:
+  virtual ~IInterface11() = 0;
+  
+protected:
+  explicit IInterface11() = default;
+
+};
+
+inline constexpr IInterface11::~IInterface11() {}
+
+} // namespace ns2
+} // namespace ns1
+
+#endif // PRJ_NS1_NS2_CLASS_HPP
